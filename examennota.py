8cab5e4a1d243ca25c00a750bdca16474d7fe819
Juanyrala03 <nachoperalta2003@gmail.com>
2025-11-24 20:02:40 -0300

    Add files via upload

---
+++ examennota.py
@@ -0,0 +1,12 @@
+print("NOTAS DE EXÁMEN")
+
+nombre = input("Ingrese su nombre y apellido:")
+nota = float(input("Ingrese la nota de su calificación:"))
+
+if (nota >= 7):
+    print("Aprobado")
+if (nota <= 6):
+    print("Ir a recuperatorio")
+if (nota == 10):
+    print("Tenes que cerrar el estadio")
+
