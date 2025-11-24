7470a8d9ccedc0b98aa4af1f561485365494503f
Juanyrala03 <nachoperalta2003@gmail.com>
2025-11-24 20:16:03 -0300

    Add files via upload
    No me salió la condición

---
+++ colesterol.py
@@ -0,0 +1,7 @@
+print("NIVEL DE COLESTEROL")
+
+colesterol = float(input("Ingrese su valor de colesterol dg/dl:"))
+if (colesterol > 190):
+    print("Amig@ controla tu boca")
+if (colesterol < 150):
+    print("Compañer@ come mucho más")
